79b3e1f32cbdc3ad1a571083fd5a83cdba731322
Mintu Sikder <mintusikder15@gmail.com>
2024-10-29 19:45:25 +0600

    added project setup

---
+++ client/eslint.config.js
@@ -0,0 +1,10 @@
+// import globals from "globals";
+// import pluginReact from "eslint-plugin-react";
+
+
+// export default [
+//   {files: ["**/*.{js,mjs,cjs,jsx}"]},
+//   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+//   {languageOptions: { globals: globals.browser }},
+//   pluginReact.configs.flat.recommended,
+// ];

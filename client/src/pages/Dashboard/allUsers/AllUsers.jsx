ddbff8a674ddd33f65580b49d1c7f13ee52594a5
Mintu Sikder <mintusikder15@gmail.com>
2024-11-08 23:25:21 +0600

    added user delete

--- client/src/pages/Dashboard/allUsers/AllUsers.jsx
+++ client/src/pages/Dashboard/allUsers/AllUsers.jsx
@@ -1,16 +1,42 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const axisSecure = useAxiosSecure();
-  const { data: users = [] } = useQuery({
+  const { data: users = [],refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axisSecure.get("/users");
       return res.data;
     },
   });
+  const handelDelete =() =>{
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axisSecure.delete(`/users/${users._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch()
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  }
   return (
     <div>
       <div className="flex justify-around">
@@ -25,6 +51,7 @@ const AllUsers = () => {
               <th></th>
               <th>Name</th>
               <th>Email</th>
+              <th>Role</th>
               <th>Action</th>
             </tr>
           </thead>
@@ -34,7 +61,23 @@ const AllUsers = () => {
                 <th>{index + 1}</th>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                <td>Blue</td>
+                <td>
+
+                <button
+                    className="btn btn-ghost btn-xs"
+                    onClick={() => handelDelete(user)}
+                  >
+                    <FaUsers className="text-red-500"></FaUsers>
+                  </button>
+                </td>
+                <td>
+                <button
+                    className="btn btn-ghost btn-xs"
+                    onClick={() => handelDelete(user)}
+                  >
+                    <FaTrashAlt className="text-red-500"></FaTrashAlt>
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
--- server/index.js
+++ server/index.js
@@ -43,7 +43,13 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+    //menu api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
